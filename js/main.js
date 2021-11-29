c5c08324087752dc21e1b865dfbbd8eaea1f0db0
GintaPr <90444892+GintaPr@users.noreply.github.com>
2021-11-29 22:47:10 +0200

    Chat and registration

--- js/main.js
+++ js/main.js
@@ -1,24 +1,91 @@
 // REGISTRATION FORM VALIDATION
-//Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+
+
+$('#submit-form').click(function() {
+  const $username = $('#username');
+  const $firstName = $('#first-name');
+  const $password = $('#password');
+  const $retypePassword = $('#retype-password');
+  const $country = $('#country');
+ 
+
+  $username
+    .removeClass('border')
+    .removeClass('border-danger');
+  $firstName
+    .removeClass('border')
+    .removeClass('border-danger');
+  $password
+    .removeClass('border')
+    .removeClass('border-danger');
+  $retypePassword
+    .removeClass('border')
+    .removeClass('border-danger');
+  $country
+    .removeClass('border')
+    .removeClass('border-danger');
+
+
+
+  if ($username.val() == '' ||$firstName.val() == '' || $password.val() == '' || $retypePassword.val() == '' || $country.val() == 'Country:') {
+      $username
+        .addClass('border')
+        .addClass('border-danger');
+      $password
+        .addClass('border')
+        .addClass('border-danger');
+      $retypePassword
+        .addClass('border')
+        .addClass('border-danger');
+      $firstName
+        .addClass('border')
+        .addClass('border-danger');
+      $country
+        .addClass('border')
+        .addClass('border-danger');
+      return false; 
+
+    } else if($password.val() !== $retypePassword.val() || $password.length() < 8) {
+      $password
+        .addClass('border')
+        .addClass('border-danger');
+      $retypePassword
+        .addClass('border')
+        .addClass('border-danger');
+alert('Password must contain at least 8 symbols')
+      return false;
+    } else {
+     true
+    }
+
+  });
 
 
 //CHAT
+$('#send-message').click(function() {
+  const $newMessageUser = $('#chat-username');
+  const $newMessage = $('#chat-message');
+  $newMessage
+      .removeClass('border')
+      .removeClass('border-danger');
+  $newMessageUser
+      .removeClass('border')
+      .removeClass('border-danger');
+
+  if ($newMessage.val() == '' || $newMessageUser.val() == '') {
+      $newMessage
+          .addClass('border')
+          .addClass('border-danger');
+      $newMessageUser
+          .addClass('border')
+          .addClass('border-danger');
+      return false;
+  }
+  const newChatMessage = `<li class="list-group-item">
+  <p><strong>${$newMessageUser.val()}</strong></p>
+  <p>${$newMessage.val()}</p></li>`;
+  $('#tasks-list').append(newChatMessage);
+  $newMessageUser.val('');
+  $newMessage.val('');
+});
